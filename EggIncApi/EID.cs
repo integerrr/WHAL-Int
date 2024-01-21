3ee7462576dd6adae6e0b24705371973d9f84984
integer <7279624+integerrr@users.noreply.github.com>
2024-01-21 12:07:02 +0000

    add some basic struture

---
+++ EggIncApi/EID.cs
@@ -0,0 +1,6 @@
+namespace WHAL_Int.EggIncApi;
+
+public partial class BasicRequestInfoParameters {
+    public const string EID = "EI1234567890123456";
+
+}
---
+++ EggIncApi/Request.cs
@@ -0,0 +1,14 @@
+namespace WHAL_Int.EggIncApi;
+
+public class Request
+{
+
+}
+
+public partial class BasicRequestInfoParameters
+{
+    const uint CLIENT_VERSION = 62;
+    const string VERSION = "1.29.1";
+    const string BUILD = "111279";
+    const string PLATFORM = "IOS";
+}
