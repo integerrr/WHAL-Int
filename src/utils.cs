839b40a5ecba41a5a41c56ab0f13ae7df67adf6f
AllTerrainWHALE <63456304+AllTerrainWHALE@users.noreply.github.com>
2024-01-24 20:38:46 +0000

    Created `utils` class for useful methods. Current implementations convert from Unix timestamp to `DateTime` object, and vise-versa

---
+++ src/utils.cs
@@ -0,0 +1,31 @@
+namespace WHAL_Int.Maj;
+
+public class utils
+{
+    /// <summary>
+    /// Converts a Unix Epoch timestamp into a <c>DateTime</c> format
+    /// </summary>
+    /// <param name="timestamp"></param>
+    /// <returns>
+    /// <c>DateTime</c> object of the time passed in
+    /// </returns>
+    public static DateTime ConvertFromUnixTimestamp(double timestamp)
+    {
+        DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        return origin.AddSeconds(timestamp);
+    }
+
+    /// <summary>
+    /// Converts a <c>DateTime</c> format into a Unix Epoch timestamp
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns>
+    /// <c>Double</c> Unix Epoch timestamp
+    /// </returns>
+    public static double ConvertToUnixTimestamp(DateTime date)
+    {
+        DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        TimeSpan diff = date.ToUniversalTime() - origin;
+        return Math.Floor(diff.TotalSeconds);
+    }
+}
