172d74aed098cdeda1c0b3817020102f85826866
Menno <Redacted>
2024-01-26 15:44:24 +0100

    Use new CoopBuilder to make fetching coops async. Example in Program.cs. Change some methods into properties and use LINQ in coops

---
+++ src/CoopBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WHAL_Int.EggIncApi;
+using WHAL_Int.Maj;
+
+namespace WHAL_Int.src;
+public class CoopBuilder
+{
+    private readonly string contractId;
+    private readonly string coopCode;
+
+    public CoopBuilder(string contractId, string coopCode)
+    {
+        this.contractId = contractId;
+        this.coopCode = coopCode;
+    }
+
+    public async Task<Coop> Build()
+    {
+        var coopStatus = await Request.GetCoopStatus(contractId, coopCode);
+        return new Coop(coopStatus);
+    }
+}
