18be729f32fdb2a6dce35c219c580df57b762798
integer <7279624+integerrr@users.noreply.github.com>
2024-01-28 01:27:23 +0000

    Add `DiscordTimestamp` class

---
+++ src/Formatter/DiscordTimestamp.cs
@@ -0,0 +1,39 @@
+namespace WHAL_Int.Formatter;
+
+public class DiscordTimestamp
+{
+    public int UnixSeconds { get; set; } = 0;
+
+    public DiscordTimestamp(int secondsSinceEpoch)
+    {
+        UnixSeconds = secondsSinceEpoch;
+    }
+
+    public string Display(DiscordTimestampDisplay displayMode)
+    {
+        char identifier = displayMode switch
+        {
+            DiscordTimestampDisplay.ShortDate => 'd',
+            DiscordTimestampDisplay.FullDate => 'D',
+            DiscordTimestampDisplay.HourMinuteTime => 't',
+            DiscordTimestampDisplay.HourMinuteSecondTime => 'T',
+            DiscordTimestampDisplay.FullDateTime => 'f',
+            DiscordTimestampDisplay.FullDateTimeDayOfWeek => 'F',
+            DiscordTimestampDisplay.Relative => 'R',
+            _ => 'f'
+        };
+
+        return $"`<t:{UnixSeconds}:{identifier}>`";
+    }
+}
+
+public enum DiscordTimestampDisplay
+{
+    ShortDate,
+    FullDate,
+    HourMinuteTime,
+    HourMinuteSecondTime,
+    FullDateTime,
+    FullDateTimeDayOfWeek,
+    Relative,
+}
