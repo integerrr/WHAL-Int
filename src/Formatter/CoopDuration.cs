b9549e8a876e6b6b93c46365c710018d0d778b2c
integer <7279624+integerrr@users.noreply.github.com>
2024-02-03 18:51:01 +0000

    Implement formatting durations

--- src/Formatter/CoopDuration.cs
+++ src/Formatter/CoopDuration.cs
@@ -2,13 +2,26 @@ namespace WHAL_Int.Formatter;
 
 public class CoopDuration : IComparable<CoopDuration>
 {
+    public const int SECONDS_IN_A_MINUTE = 60;
+    public const int SECONDS_IN_AN_HOUR = SECONDS_IN_A_MINUTE * 60;
+    public const int SECONDS_IN_A_DAY = SECONDS_IN_AN_HOUR * 24;
+
     public long DurationInSeconds { get; set; } = 0;
 
     public CoopDuration(long durationInSeconds) => DurationInSeconds = durationInSeconds;
 
     public string Format()
     {
-        throw new NotImplementedException();
+        long day = DurationInSeconds / SECONDS_IN_A_DAY;
+        long hour = (DurationInSeconds - day * SECONDS_IN_A_DAY) / SECONDS_IN_AN_HOUR;
+        long min = (DurationInSeconds - day * SECONDS_IN_A_DAY - hour * SECONDS_IN_AN_HOUR) / SECONDS_IN_A_MINUTE;
+
+        if (day > 99) return "too long";
+
+        string str = day > 0 ? $"{day}d" : "";
+        str += hour > 0 ? $"{hour}h" : "";
+        str += $"{min}m";
+        return str;
     }
 
     public int CompareTo(CoopDuration? other)
