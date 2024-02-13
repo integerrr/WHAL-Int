d1718ad42aa5223c57af2846b473eafd9b94eeee
AllTerrainWHALE <63456304+AllTerrainWHALE@users.noreply.github.com>
2024-02-13 20:19:11 +0000

    Fully implemented the sruc table, doing my best to use good C# practises instead of making it look all Python-y.
    Added a new class called `StringFormatter` while I was at it. Currently only has the ability to center text, but can add left and right alignment whenever (along with other things).
    
    I also changed the version in the `.csproj` file to be v8.0, as I was being screamed at saying that I can't use raw string literals in v6.0 and below...

--- WHAL-Int/src/Formatter/Duration.cs
+++ WHAL-Int/src/Formatter/Duration.cs
@@ -2,13 +2,30 @@ namespace WHAL_Int.Formatter;
 
 public class Duration : IComparable<Duration>
 {
-    public long DurationInSeconds { get; set; } = 0;
 
-    public Duration(long durationInSeconds) => DurationInSeconds = durationInSeconds;
+    public const int SECONDS_IN_A_MINUTE = 60;
+    public const int SECONDS_IN_AN_HOUR = SECONDS_IN_A_MINUTE * 60;
+    public const int SECONDS_IN_A_DAY = SECONDS_IN_AN_HOUR * 24;
+
+    public double DurationInSeconds { get; set; } = 0;
+
+    public Duration(double durationInSeconds) => DurationInSeconds = durationInSeconds;
 
     public string Format()
     {
-        throw new NotImplementedException();
+        // Find the total number of days, hours and minutes from the duration
+        double day = Math.Floor(DurationInSeconds / SECONDS_IN_A_DAY);
+        double hour = Math.Floor((DurationInSeconds - day * SECONDS_IN_A_DAY) / SECONDS_IN_AN_HOUR);
+        double min = Math.Floor((DurationInSeconds - day * SECONDS_IN_A_DAY - hour * SECONDS_IN_AN_HOUR) / SECONDS_IN_A_MINUTE);
+
+        // Convert into string format of dd/hh/mm
+        string coopDurationAsString = ""
+            + (day > 0 ? $"{day}d" : "")
+            + (hour > 0 ? $"{hour}h" : "")
+            + (min > 0 ? $"{min}m" : "")
+        ;
+
+        return coopDurationAsString;
     }
 
     public int CompareTo(Duration? other)
