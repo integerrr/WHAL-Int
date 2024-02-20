156da8fae88bb7e54193c035c0da58f599dc5746
AllTerrainWHALE <63456304+AllTerrainWHALE@users.noreply.github.com>
2024-02-20 13:02:31 +0000

    Removed the need for StringFormatter to be done outside of AddColumn function. It now takes an optional enum (default Centered) and width (default 5). In implementing this, I made logic for GetTable appear simpler (I think?). Hopefully that's up to your standards Int

--- WHAL-Int/src/Formatter/StringFormatter.cs
+++ WHAL-Int/src/Formatter/StringFormatter.cs
@@ -1,9 +1,18 @@
-using System;
+using System.ComponentModel;
 
 namespace WHAL_Int.Formatter;
 
 public class StringFormatter
 {
+    public static string Left(string s, int width)
+    {
+        if (s.Length >= width) return s;
+
+        int rightPadding = width - s.Length;
+
+        return s + new string(' ', rightPadding);
+    }
+
     public static string Centered(string s, int width)
     {
         if (s.Length >= width) return s;
@@ -13,4 +22,34 @@ public class StringFormatter
 
         return new string(' ', leftPadding) + s + new string(' ', rightPadding);
     }
+
+    public static string Right(string s, int width)
+    {
+        if (s.Length >= width) return s;
+
+        int leftPadding = width - s.Length;
+
+        return new string(' ', leftPadding) + s;
+    }
+
+    public static string Align(string s, int width, StringAlignment alignment)
+    {
+        return alignment switch
+        {
+            StringAlignment.Left => Left(s, width),
+            StringAlignment.Centered => Centered(s, width),
+            StringAlignment.Right => Right(s, width),
+            StringAlignment.None => s,
+
+            _ => throw new InvalidEnumArgumentException()
+        };
+    }
+}
+
+public enum StringAlignment
+{
+    Left,
+    Centered,
+    Right,
+    None
 }
--- WHAL-Int/src/Formatter/Table.cs
+++ WHAL-Int/src/Formatter/Table.cs
@@ -1,21 +1,34 @@
+using System.Drawing;
 using WHAL_Int.Maj;
 
 namespace WHAL_Int.Formatter;
 
 public class Table<T>
 {
-    private readonly Dictionary<string, Func<T, string>> columns = new();
+    private readonly Dictionary<string, (Func<T, string>, int, StringAlignment)> columns = new();
     private readonly List<T> dataPoints = new();
 
-    /*public Table(List<T> dataPoints) => this.dataPoints = dataPoints;*/
+    public void AddColumn(
+        string title,
+        Func<T, string> colFunc,
+        int colWidth = 5,
+        StringAlignment alignment = StringAlignment.Centered
+    ) => columns.Add(title, (colFunc, colWidth, alignment));
 
-    public void AddColumn(string title, Func<T, string> colFunc) => columns.Add(title, colFunc);
     public void AddDataPoint(T dataPoint) => dataPoints.Add(dataPoint);
 
     public string GetTable()
     {
+        /*
         string table = string.Join("|", columns.Keys.Select(c => c)) + "\n"
             + string.Join("\n", dataPoints.Select(x => string.Join("|", columns.Values.Select(f => f(x)))));
+        */
+
+        string table = string.Join("|", columns.Keys.Select(c => c)) + "\n";
+        foreach (T point in dataPoints)
+        {
+            table += string.Join("|", columns.Values.Select(f => StringFormatter.Align(f.Item1(point), f.Item2, f.Item3))) + "\n";
+        }
 
         return table;
     }
